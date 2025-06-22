12fa274accaef2cfaa3bc968f56c5ac56d32c25c
Adrien ESCOUBEYROU <adrien.escoubeyrou@gmail.com>
2025-06-22 23:33:47 +0200

    modification for deployment

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { Server } = require("socket.io");
 const PORT = process.env.PORT || 3001;
 const io = new Server(3001, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://adrienescbr.github.io/dekou-client/",
     methods: ["GET", "POST"],
   },
 });
